6224b825d24443fe11d1b4ddba2af93a295a9788
julia-kliot <juliakliot.jk@gmail.com>
2022-05-08 19:56:27 +0300

    launch

---
+++ src/test/java/testwiki/LaunchWiki.java
@@ -0,0 +1,14 @@
+package testwiki;
+
+import configwiki.ConfigurationWiki;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LaunchWiki extends ConfigurationWiki {
+
+    @Test
+    public void lauchWiki(){
+
+
+    }
+}
